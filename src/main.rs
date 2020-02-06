26819ed4fea25912daf20116c9cfa23dd236570b
Charles Fenwick Elliott <charles@fenwickelliott.io>
2020-02-06 09:35:47 -0500

    comparison

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -11,6 +12,14 @@ fn main() {
     
     println!(">");
     std::io::stdin().read_line(&mut guess).expect("Failed to read line");
+    
+    let guess: u32 = guess.trim().parse().expect("fatal: input was not an integer");
+
+    match guess.cmp(&secret) {
+        Ordering::Less => println!("too small"),
+        Ordering::Equal => println!("Horray!"),
+        Ordering::Greater => println!("too big"),
+    }
 
     println!("> {}", guess)
 
