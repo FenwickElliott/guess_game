3bdc4afdb521d5225fc0c3d9c82c0ab4e70f53c2
Charles Fenwick Elliott <charles@fenwickelliott.io>
2020-02-06 09:32:45 -0500

    rand

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
-
+    
+    let secret = rand::thread_rng().gen_range(1, 101);
     let mut guess = String::new();
-
+    
+    
+    println!("Guess the number!");
+    println!("ans: {}", secret);
+    
     println!(">");
-
     std::io::stdin().read_line(&mut guess).expect("Failed to read line");
 
     println!("> {}", guess)
