220fc7fe7b95a49e238cd1e9b1d34931f77c0a29
Charles Fenwick Elliott <charles@fenwickelliott.io>
2020-02-09 11:25:39 -0500

    error handling and for loop

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,41 @@
 use std::cmp::Ordering;
+use std::io::{self, Write};
 use rand::Rng;
 
 fn main() {
     
-    let secret = rand::thread_rng().gen_range(1, 101);
-    let mut guess = String::new();
-    
-    
-    println!("Guess the number!");
-    println!("ans: {}", secret);
-    
-    loop {
-        println!(">");
-        std::io::stdin().read_line(&mut guess).expect("Failed to read line");
-        
-        let guess: u32 = guess.trim().parse().expect("fatal: input was not a positive integer");
+    let ans = rand::thread_rng().gen_range(1, 101);
 
-        println!("> {}", guess);
-        match guess.cmp(&secret) {
+    println!("Guess the number! (You have 10 guesses)");
+    println!("ans: {}", ans);
+    
+    for _ in 0..10 {
+        let mut guess = String::new();
+        
+        print!("> ");
+        io::stdout().flush().unwrap();
+        
+        io::stdin().read_line(&mut guess)
+        .ok()
+        .expect("fatal: failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("input was not a positive integer, please try again");
+                continue
+            }
+        };
+
+        match guess.cmp(&ans) {
             Ordering::Less => println!("too small"),
             Ordering::Equal => {
-                println!("Horray!");
-                break;
+                println!("Horray! {} was correct", guess);
+                return;
             }
             Ordering::Greater => println!("too big"),
         }
-
     }
 
+    println!("I'm sorry, you're out of guesses. The answer was: {}", ans);
 }
